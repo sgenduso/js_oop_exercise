1ad5fed41ab68556eaf5f8ce3d4fa1d8b30aa452
Sam Genduso <sam.genduso@gmail.com>
2015-07-13 10:30:35 -0600

    first 2 tests pass

--- spec/animalSpec.js
+++ spec/animalSpec.js
@@ -5,27 +5,28 @@ describe('Animal', function(){
 
   beforeEach(function(){
     // create a new animal object each time
-    animal = new Animal("Fido", 27, "Pig");
+    animal = new Animal("Fido", 27, "pig");
   });
 
   describe('#oink', function(){
     it('should oink if it is a pig', function(){
-
+      expect(animal.oink()).toEqual('oink');
     });
     it('should not oink if it is not pig', function(){
-
+      animal.kind = 'lion';
+      expect(animal.oink()).toEqual('HEY, I am not a pig!');
     });
   });
 
   describe('#growUp', function(){
     it('should increment the age by one', function(){
-
+      expect(animal.growUp()).toEqual(28);
     });
   });
 
   describe('#awake', function(){
     it('should initially be off', function(){
-
+      expect(animal.awake).toEqual(false);
     });
   });
 
@@ -50,5 +51,3 @@ describe('Animal', function(){
     });
   });
 });
-
-
--- src/animal.js
+++ src/animal.js
@@ -1,9 +1,22 @@
-function Animal(){
-
+function Animal(name, age, kind, awake){
+  this.name = name;
+  this.age = age;
+  this.kind = kind;
+  this.awake = false;
 }
 
 Animal.prototype.oink = function(){
-
+  if (this.kind === 'pig') {
+    return 'oink';
+  } else {
+    return 'HEY, I am not a pig!';
+  }
 };
+
+Animal.prototype.growUp = function () {
+  this.age++;
+  return this.age;
+};
+
 
 module.exports=Animal;
